29d4dac099d36aaaefe24ccfb57b643dd405599b
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-12-13 16:07:37 +0100

    improved performance on desktop

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        ssr: {
-            noExternal: ['styles', 'data']
-        }
+  vite: {
+    ssr: {
+      noExternal: ['styles', 'data']
     }
+  },
 });
